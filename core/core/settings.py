845b1c591d305d263765e70ab978c40994eb0de4
Pradum <pradumchaurasiya99856@gmail.com>
2025-11-02 20:35:49 +0530

    ..

--- core/core/settings.py
+++ core/core/settings.py
@@ -69,9 +69,18 @@ TEMPLATES = [
 ]
 ASGI_APPLICATION = 'core.asgi.application'
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     },
+# }
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
