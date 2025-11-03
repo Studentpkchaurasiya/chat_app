176d3818e0c98e8b39d6638608f637f0b0c2d0e6
Pradum <pradumchaurasiya99856@gmail.com>
2025-11-03 11:57:00 +0530

    ..

--- core/chat/urls.py
+++ core/chat/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("chat/", views.chat_view, name="personal_contact"),
+    path("chat/", views.personal_contact, name="personal_contact"),
     path("chat/<str:username>/", views.chat_view, name="chat"),
 ]
